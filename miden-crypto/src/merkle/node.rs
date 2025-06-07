87b2c34776cc1bdf042bacdbc1d84e0cf032f7a9
Himess <95512809+Himess@users.noreply.github.com>
2025-06-07 18:30:06 +0300

    nit

--- miden-crypto/src/merkle/node.rs
+++ miden-crypto/src/merkle/node.rs
@@ -5,7 +5,7 @@ use super::Word;
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 #[cfg_attr(test, derive(PartialOrd, Ord))]
 pub struct InnerNodeInfo {
-    /// The value stored in this node (typically a hash).
+    /// The value stored in this node a hash.
     pub value: Word,
 
     /// The left child node.
