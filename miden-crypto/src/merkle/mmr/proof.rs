e73427d0a0e0da69c3c913fad7f7fd1b7639e132
Himess <95512809+Himess@users.noreply.github.com>
2025-06-09 13:33:18 +0300

    nit

--- miden-crypto/src/merkle/mmr/proof.rs
+++ miden-crypto/src/merkle/mmr/proof.rs
@@ -5,11 +5,11 @@ use super::forest::Forest;
 // MMR PROOF
 // ================================================================================================
 
-#[derive(Debug, Clone, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 /// Represents a proof for a leaf in a Merkle Mountain Range (MMR).
 ///
 /// Contains the leaf's global position, the forest state, and the associated Merkle path.
+#[derive(Debug, Clone, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 pub struct MmrProof {
     /// The state of the MMR when the MmrProof was created.
     pub forest: Forest,
