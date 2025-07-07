1a6f092c29a707b023d1645f5ce53e46f5063a20
sergerad <sergerad@miden.team>
2025-07-07 19:15:11 +1200

    Lint

--- miden-crypto/src/lib.rs
+++ miden-crypto/src/lib.rs
@@ -24,14 +24,14 @@ pub use word::{Word, WordError};
 
 /// An alias for a key-value map.
 ///
-/// By default, this is an alias for the standard [BTreeMap], however, when the `hashmaps` feature
+/// By default, this is an alias for the standard [`BTreeMap`], however, when the `hashmaps` feature
 /// is enabled, this is an alias for the `hashbrown`'s `HashMap`.
 #[cfg(feature = "hashmaps")]
 pub type Map<K, V> = hashbrown::HashMap<K, V>;
 
 /// An alias for a key-value map.
 ///
-/// By default, this is an alias for the standard [BTreeMap], however, when the `hashmaps` feature
+/// By default, this is an alias for the standard [`BTreeMap`], however, when the `hashmaps` feature
 /// is enabled, this is an alias for the `hashbrown`'s `HashMap`.
 #[cfg(not(feature = "hashmaps"))]
 pub type Map<K, V> = alloc::collections::BTreeMap<K, V>;
