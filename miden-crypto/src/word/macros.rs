d6be776e740ed9d0e0f0937c0bbac44c24d4952f
sergerad <sergerad@miden.team>
2025-07-13 22:00:55 +1200

    Move parse_from_hex_string() to Word::parse()

--- miden-crypto/src/word/macros.rs
+++ miden-crypto/src/word/macros.rs
@@ -1,5 +1,3 @@
-use super::{Felt, StarkField, Word};
-
 // MACROS
 // ================================================================================================
 
@@ -9,73 +7,11 @@ use super::{Felt, StarkField, Word};
 #[macro_export]
 macro_rules! word {
     ($hex:expr) => {{
-        let word: Word = match $crate::word::parse_hex_string_as_word($hex) {
+        let word: Word = match $crate::word::Word::parse($hex) {
             Ok(v) => v,
             Err(e) => panic!("{}", e),
         };
 
         word
     }};
-}
-
-/// Parses a hex string into a [`Word`] array.
-pub const fn parse_hex_string_as_word(hex: &str) -> Result<Word, &'static str> {
-    const fn parse_hex_digit(digit: u8) -> Result<u8, &'static str> {
-        match digit {
-            b'0'..=b'9' => Ok(digit - b'0'),
-            b'A'..=b'F' => Ok(digit - b'A' + 0x0a),
-            b'a'..=b'f' => Ok(digit - b'a' + 0x0a),
-            _ => Err("Invalid hex character"),
-        }
-    }
-    // Enforce and skip the '0x' prefix.
-    let hex_bytes = match hex.as_bytes() {
-        [b'0', b'x', rest @ ..] => rest,
-        _ => return Err("Hex string must have a \"0x\" prefix"),
-    };
-
-    if hex_bytes.len() > 64 {
-        return Err("Hex string has more than 64 characters");
-    }
-
-    let mut felts = [0u64; 4];
-    let mut i = 0;
-    while i < hex_bytes.len() {
-        let hex_digit = match parse_hex_digit(hex_bytes[i]) {
-            // SAFETY: u8 cast to u64 is safe. We cannot use u64::from in const context so we
-            // are forced to cast.
-            Ok(v) => v as u64,
-            Err(e) => return Err(e),
-        };
-
-        // This digit's nibble offset within the felt. We need to invert the nibbles per
-        // byte for endianness reasons i.e. ABCD -> BADC.
-        let inibble = if i.is_multiple_of(2) {
-            (i + 1) % 16
-        } else {
-            (i - 1) % 16
-        };
-
-        let value = hex_digit << (inibble * 4);
-        felts[i / 2 / 8] += value;
-
-        i += 1;
-    }
-
-    // Ensure each felt is within bounds as `Felt::new` silently wraps around.
-    // This matches the behavior of `Word::try_from(String)`.
-    let mut idx = 0;
-    while idx < felts.len() {
-        if felts[idx] >= Felt::MODULUS {
-            return Err("Felt overflow");
-        }
-        idx += 1;
-    }
-
-    Ok(Word::new([
-        Felt::new(felts[0]),
-        Felt::new(felts[1]),
-        Felt::new(felts[2]),
-        Felt::new(felts[3]),
-    ]))
 }
--- miden-crypto/src/word/mod.rs
+++ miden-crypto/src/word/mod.rs
@@ -26,7 +26,6 @@ use crate::{
 };
 
 mod macros;
-pub use macros::parse_hex_string_as_word;
 
 mod lexicographic;
 pub use lexicographic::LexicographicWord;
@@ -50,6 +49,68 @@ impl Word {
     /// Creates a new [Word] from the given field elements.
     pub const fn new(value: [Felt; WORD_SIZE_FELT]) -> Self {
         Self(value)
+    }
+
+    /// Parses a hex string into a [`Word`] array.
+    pub const fn parse(hex: &str) -> Result<Word, &'static str> {
+        const fn parse_hex_digit(digit: u8) -> Result<u8, &'static str> {
+            match digit {
+                b'0'..=b'9' => Ok(digit - b'0'),
+                b'A'..=b'F' => Ok(digit - b'A' + 0x0a),
+                b'a'..=b'f' => Ok(digit - b'a' + 0x0a),
+                _ => Err("Invalid hex character"),
+            }
+        }
+        // Enforce and skip the '0x' prefix.
+        let hex_bytes = match hex.as_bytes() {
+            [b'0', b'x', rest @ ..] => rest,
+            _ => return Err("Hex string must have a \"0x\" prefix"),
+        };
+
+        if hex_bytes.len() > 64 {
+            return Err("Hex string has more than 64 characters");
+        }
+
+        let mut felts = [0u64; 4];
+        let mut i = 0;
+        while i < hex_bytes.len() {
+            let hex_digit = match parse_hex_digit(hex_bytes[i]) {
+                // SAFETY: u8 cast to u64 is safe. We cannot use u64::from in const context so we
+                // are forced to cast.
+                Ok(v) => v as u64,
+                Err(e) => return Err(e),
+            };
+
+            // This digit's nibble offset within the felt. We need to invert the nibbles per
+            // byte for endianness reasons i.e. ABCD -> BADC.
+            let inibble = if i.is_multiple_of(2) {
+                (i + 1) % 16
+            } else {
+                (i - 1) % 16
+            };
+
+            let value = hex_digit << (inibble * 4);
+            felts[i / 2 / 8] += value;
+
+            i += 1;
+        }
+
+        // Ensure each felt is within bounds as `Felt::new` silently wraps around.
+        // This matches the behavior of `Word::try_from(String)`.
+        let mut idx = 0;
+        while idx < felts.len() {
+            if felts[idx] >= Felt::MODULUS {
+                return Err("Felt overflow");
+            }
+            idx += 1;
+        }
+
+        Ok(Word::new([
+            Felt::new(felts[0]),
+            Felt::new(felts[1]),
+            Felt::new(felts[2]),
+            Felt::new(felts[3]),
+        ]))
     }
 
     /// Returns a new [Word] consisting of four ZERO elements.
