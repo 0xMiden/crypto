1b221b447ee631c78a546da863851ddb72f0a4f6
Al-Kindi-0 <82364884+Al-Kindi-0@users.noreply.github.com>
2025-09-01 13:38:15 +0200

    remove AEAD trait and impls

--- miden-crypto/src/encryption/mod.rs
+++ miden-crypto/src/encryption/mod.rs
@@ -1,15 +1,6 @@
 //! AEAD (authenticated encryption with associated data) schemes.
 
-use alloc::vec::Vec;
-
-use rand::{CryptoRng, RngCore};
 use thiserror::Error;
-use zeroize::Zeroize;
-
-use crate::{
-    Felt,
-    utils::{Deserializable, Serializable},
-};
 
 pub mod aead_rpo;
 
@@ -21,69 +12,8 @@ pub mod xchacha;
 /// Number of bytes to pack into one field element
 const BINARY_CHUNK_SIZE: usize = 7;
 
-// TRAITS
-// ================================================================================================
-
-/// Authenticated encryption with associated data (AEAD) scheme
-pub trait AeadMiden {
-    type Key: Zeroize;
-    type Nonce: Clone + Serializable + Deserializable;
-
-    /// Derive an encryption key from shared secret and salt using KDF
-    fn derive_key(shared_secret: &[u8], salt: &[u8]) -> Self::Key;
-
-    /// Generate a random nonce
-    fn generate_nonce<R: CryptoRng + RngCore>(rng: &mut R) -> Self::Nonce;
-
-    /// Encrypt plaintext, as Felt-s, with associated data
-    fn encrypt(
-        key: &Self::Key,
-        nonce: Self::Nonce,
-        plaintext: &[Felt],
-        associated_data: &[Felt],
-    ) -> Result<Vec<Felt>, IesError>;
-
-    /// Decrypt ciphertext, as Felt-s, with associated data
-    fn decrypt(
-        key: &Self::Key,
-        nonce: Self::Nonce,
-        ciphertext: &[Felt],
-        associated_data: &[Felt],
-    ) -> Result<Vec<Felt>, IesError>;
-
-    /// Encrypt plaintext, as bytes, with associated data
-    fn encrypt_bytes(
-        key: &Self::Key,
-        nonce: Self::Nonce,
-        plaintext: &[u8],
-        associated_data: &[u8],
-    ) -> Result<Vec<u8>, IesError>;
-
-    /// Decrypt ciphertext, as bytes, with associated data
-    fn decrypt_bytes(
-        key: &Self::Key,
-        nonce: Self::Nonce,
-        ciphertext: &[u8],
-        associated_data: &[u8],
-    ) -> Result<Vec<u8>, IesError>;
-}
-
 // ERROR TYPES
 // ================================================================================================
-
-#[derive(Debug, Error)]
-pub enum IesError {
-    #[error("key agreement failed")]
-    KeyAgreementFailed,
-    #[error("encryption failed")]
-    EncryptionFailed,
-    #[error("decryption failed")]
-    DecryptionFailed,
-    #[error("invalid key size failed")]
-    InvalidKeySize,
-    #[error("invalid nonce size failed")]
-    InvalidNonceSize,
-}
 
 /// Errors that can occur during encryption/decryption operations
 #[derive(Debug, Error)]
--- miden-crypto/src/encryption/xchacha/mod.rs
+++ miden-crypto/src/encryption/xchacha/mod.rs
@@ -3,14 +3,15 @@ use alloc::vec::Vec;
 use chacha20poly1305::{
     XChaCha20Poly1305,
     aead::{Aead, AeadCore, KeyInit},
-    consts::U10,
 };
 use rand::{CryptoRng, RngCore};
 use zeroize::Zeroize;
 
+#[cfg(feature = "std")]
+use crate::encryption::EncryptionError;
 use crate::{
     Felt,
-    encryption::{AeadMiden, BINARY_CHUNK_SIZE, IesError},
+    encryption::BINARY_CHUNK_SIZE,
     utils::{ByteReader, ByteWriter, Deserializable, DeserializationError, Serializable},
 };
 
@@ -67,7 +68,7 @@ impl SecretKey {
         &self,
         data: &[Felt],
         associated_data: &[Felt],
-    ) -> Result<EncryptedDataFelt, IesError> {
+    ) -> Result<EncryptedDataFelt, EncryptionError> {
         let mut rng = rand::rng();
 
         let nonce = Nonce::with_rng(&mut rng);
@@ -80,7 +81,7 @@ impl SecretKey {
         data: &[Felt],
         associated_data: &[Felt],
         nonce: Nonce,
-    ) -> Result<EncryptedDataFelt, IesError> {
+    ) -> Result<EncryptedDataFelt, EncryptionError> {
         let data_byte = felts_to_bytes(data);
         let ad_byte = felts_to_bytes(associated_data);
         let payload = chacha20poly1305::aead::Payload { msg: &data_byte, aad: &ad_byte };
@@ -89,7 +90,7 @@ impl SecretKey {
 
         let ciphertext = cipher
             .encrypt(&nonce.inner, payload)
-            .map_err(|_| (IesError::EncryptionFailed))?;
+            .map_err(|_| (EncryptionError::FailedOperation))?;
 
         let ciphertext_felt = bytes_to_felts(&ciphertext);
         let nonce_felt = bytes_to_felts(nonce.inner.as_slice()).try_into().unwrap();
@@ -107,7 +108,7 @@ impl SecretKey {
         &self,
         data: &[u8],
         associated_data: &[u8],
-    ) -> Result<EncryptedData, IesError> {
+    ) -> Result<EncryptedData, EncryptionError> {
         let mut rng = rand::rng();
         let nonce = Nonce::with_rng(&mut rng);
 
@@ -120,14 +121,14 @@ impl SecretKey {
         data: &[u8],
         associated_data: &[u8],
         nonce: Nonce,
-    ) -> Result<EncryptedData, IesError> {
+    ) -> Result<EncryptedData, EncryptionError> {
         let payload = chacha20poly1305::aead::Payload { msg: data, aad: associated_data };
 
         let cipher = XChaCha20Poly1305::new(&self.0.into());
 
         let ciphertext = cipher
             .encrypt(&nonce.inner, payload)
-            .map_err(|_| (IesError::EncryptionFailed))?;
+            .map_err(|_| (EncryptionError::FailedOperation))?;
 
         Ok(EncryptedData {
             associated_data: associated_data.into(),
@@ -137,7 +138,10 @@ impl SecretKey {
     }
 
     /// Decrypts the provided encrypted data using this secret key
-    pub fn decrypt(&self, encrypted_data: &EncryptedDataFelt) -> Result<Vec<Felt>, IesError> {
+    pub fn decrypt(
+        &self,
+        encrypted_data: &EncryptedDataFelt,
+    ) -> Result<Vec<Felt>, EncryptionError> {
         let EncryptedDataFelt { associated_data, ciphertext, nonce } = encrypted_data;
         let associated_data = &felts_to_bytes(associated_data);
         let ciphertext = &felts_to_bytes(ciphertext);
@@ -147,21 +151,27 @@ impl SecretKey {
 
         let cipher = XChaCha20Poly1305::new(&self.0.into());
 
-        let plaintext =
-            cipher.decrypt(&nonce.inner, payload).map_err(|_| IesError::DecryptionFailed)?;
+        let plaintext = cipher
+            .decrypt(&nonce.inner, payload)
+            .map_err(|_| EncryptionError::FailedOperation)?;
         let plaintext_felt = bytes_to_felts(&plaintext);
 
         Ok(plaintext_felt)
     }
 
     /// Decrypts the provided encrypted data, as bytes, using this secret key
-    pub fn decrypt_bytes(&self, encrypted_data: &EncryptedData) -> Result<Vec<u8>, IesError> {
+    pub fn decrypt_bytes(
+        &self,
+        encrypted_data: &EncryptedData,
+    ) -> Result<Vec<u8>, EncryptionError> {
         let EncryptedData { associated_data, ciphertext, nonce } = encrypted_data;
         let payload = chacha20poly1305::aead::Payload { msg: ciphertext, aad: associated_data };
 
         let cipher = XChaCha20Poly1305::new(&self.0.into());
 
-        cipher.decrypt(&nonce.inner, payload).map_err(|_| IesError::DecryptionFailed)
+        cipher
+            .decrypt(&nonce.inner, payload)
+            .map_err(|_| EncryptionError::FailedOperation)
     }
 }
 
@@ -210,85 +220,6 @@ impl EncryptedDataFelt {
     /// Returns the associated data to the encrypted data.
     pub fn associated_data(&self) -> &[Felt] {
         &self.associated_data
-    }
-
-    fn as_felts(&self) -> Vec<Felt> {
-        let mut result =
-            Vec::with_capacity(self.associated_data.len() + self.ciphertext.len() + 3 + 3);
-        result.push(Felt::new(self.associated_data.len() as u64));
-        result.extend_from_slice(self.associated_data());
-        result.push(Felt::new(self.ciphertext.len() as u64));
-        result.extend_from_slice(&self.ciphertext);
-        result.extend_from_slice(&self.nonce);
-
-        result
-    }
-}
-
-pub struct XChaCha20Poly1305Encryption;
-
-impl AeadMiden for XChaCha20Poly1305Encryption {
-    type Key = SecretKey;
-    type Nonce = Nonce;
-
-    fn derive_key(shared_secret: &[u8], _salt: &[u8]) -> Self::Key {
-        let key_bytes =
-            chacha20::hchacha::<U10>(shared_secret.as_ref().into(), [0_u8; 16].as_ref().into());
-        SecretKey(key_bytes.into())
-    }
-
-    fn generate_nonce<R: CryptoRng + RngCore>(rng: &mut R) -> Self::Nonce {
-        Nonce::with_rng(rng)
-    }
-
-    fn encrypt(
-        key: &Self::Key,
-        nonce: Self::Nonce,
-        plaintext: &[Felt],
-        associated_data: &[Felt],
-    ) -> Result<Vec<Felt>, IesError> {
-        let encrypted_data = key.encrypt_with_nonce(plaintext, associated_data, nonce)?;
-        Ok(encrypted_data.as_felts())
-    }
-
-    fn decrypt(
-        key: &Self::Key,
-        nonce: Self::Nonce,
-        ciphertext: &[Felt],
-        associated_data: &[Felt],
-    ) -> Result<Vec<Felt>, IesError> {
-        let encrypted_data = &EncryptedDataFelt {
-            associated_data: associated_data.to_vec(),
-            ciphertext: ciphertext.to_vec(),
-            nonce: bytes_to_felts(nonce.inner.as_slice()).try_into().unwrap(),
-        };
-        let decrypted_data = key.decrypt(encrypted_data)?;
-        Ok(decrypted_data)
-    }
-
-    fn encrypt_bytes(
-        key: &Self::Key,
-        nonce: Self::Nonce,
-        plaintext: &[u8],
-        associated_data: &[u8],
-    ) -> Result<Vec<u8>, IesError> {
-        let encrypted_data = key.encrypt_bytes_with_nonce(plaintext, associated_data, nonce)?;
-        Ok(encrypted_data.to_bytes())
-    }
-
-    fn decrypt_bytes(
-        key: &Self::Key,
-        nonce: Self::Nonce,
-        ciphertext: &[u8],
-        associated_data: &[u8],
-    ) -> Result<Vec<u8>, IesError> {
-        let encrypted_data = &EncryptedData {
-            associated_data: associated_data.to_vec(),
-            ciphertext: ciphertext.to_vec(),
-            nonce,
-        };
-        let decrypted_data = key.decrypt_bytes(encrypted_data)?;
-        Ok(decrypted_data)
     }
 }
 
