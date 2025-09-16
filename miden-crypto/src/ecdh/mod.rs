334ef88ff5b6540797e2fbdeaa2ece32bda9e142
al-kin <82364884+Al-Kindi-0@users.noreply.github.com>
2025-09-16 16:28:44 +0400

    feat: add digital signature and key exchange using curve 25519

--- miden-crypto/src/ecdh/mod.rs
+++ miden-crypto/src/ecdh/mod.rs
@@ -2,3 +2,8 @@
 
 mod k256;
 pub use k256::{EphemeralPublicKey, EphemeralSecretKey, SharedSecret};
+mod x25519;
+pub use x25519::{
+    EphemeralPublicKey as X25519EphemeralPublicKey, EphemeralSecretKey as X25519EphemeralSecretKey,
+    SharedSecret as X25519SharedSecret,
+};
