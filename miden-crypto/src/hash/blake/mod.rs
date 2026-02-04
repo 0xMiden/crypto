1635124e9d3ccdf6777b098657e72b5c3a8c610d
Farukest <abdullahfarukozden@gmail.com>
2026-02-04 07:47:25 +0300

    fix: remove unused imports in blake mod for no-default-features

--- miden-crypto/src/hash/blake/mod.rs
+++ miden-crypto/src/hash/blake/mod.rs
@@ -2,15 +2,11 @@ use core::mem::size_of;
 
 use super::{
     HasherExt,
-    digest::{Digest, Digest192, Digest256}
+    digest::{Digest, Digest192, Digest256},
 };
 use crate::{
     Felt,
     field::{BasedVectorSpace, PrimeField64},
-    utils::{
-        ByteReader, ByteWriter, Deserializable, DeserializationError, HexParseError, Serializable,
-        bytes_to_hex_string, hex_to_bytes,
-    },
 };
 
 #[cfg(test)]
