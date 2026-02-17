b818cccee4aaf73bd4eff8edb07f0917119d85fc
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2026-02-17 09:14:24 +0200

    fix: re-export `miden_field::word` module from `miden-crypto`

--- miden-crypto/src/lib.rs
+++ miden-crypto/src/lib.rs
@@ -16,10 +16,9 @@ pub mod merkle;
 pub mod rand;
 pub mod utils;
 
-use miden_field::word;
 // RE-EXPORTS
 // ================================================================================================
-pub use miden_field::{Felt, LexicographicWord, Word, WordError};
+pub use miden_field::{Felt, LexicographicWord, Word, WordError, word};
 
 pub mod field {
     //! Traits and utilities for working with the Goldilocks finite field (i.e.,
