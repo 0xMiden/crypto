c9d9d48225aa67cf90bcb6bc22853238c027fc4e
bobbinth <bobbinth@users.noreply.github.com>
2026-03-04 19:09:24 +0000

    deploy: e42154ac0c504693d29980664fc63c100c380a23

--- docs/miden_crypto/rand/sidebar-items.js
+++ docs/miden_crypto/rand/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["random_felt","random_word"],"mod":["test_utils"],"struct":["RpoRandomCoin","RpxRandomCoin"],"trait":["FeltRng","Randomizable"]};
+window.SIDEBAR_ITEMS = {"fn":["random_felt","random_word"],"mod":["test_utils"],"struct":["RandomCoin"],"trait":["FeltRng","Randomizable"]};
