285d1ee5b60e454738b0255db88cc557d03b4128
François Garillot <francois@garillot.net>
2026-04-14 01:50:57 -0400

    chore(clippy): add curated lint set and fix violations

--- miden-crypto/src/dsa/falcon512_poseidon2/keys/secret_key.rs
+++ miden-crypto/src/dsa/falcon512_poseidon2/keys/secret_key.rs
@@ -389,10 +389,10 @@ impl Deserializable for SecretKey {
         // big_g * f - g * big_f = p (mod X^n + 1)
         let big_g = g.fft().hadamard_div(&f.fft()).hadamard_mul(&big_f.fft()).ifft();
         let basis = [
-            g.map(|f| f.balanced_value()),
-            -f.map(|f| f.balanced_value()),
-            big_g.map(|f| f.balanced_value()),
-            -big_f.map(|f| f.balanced_value()),
+            Polynomial::new(g.to_balanced_values()),
+            -Polynomial::new(f.to_balanced_values()),
+            Polynomial::new(big_g.to_balanced_values()),
+            -Polynomial::new(big_f.to_balanced_values()),
         ];
         Ok(Self::from_short_lattice_basis(basis))
     }
